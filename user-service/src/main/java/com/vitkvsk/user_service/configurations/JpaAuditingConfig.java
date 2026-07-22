39f940b97eafd603c26aa047bf5bd801514750c5
Maksim Vitkouski <mvit4725@gmail.com>
2026-07-22 13:37:59 +0300

    JpaAuditingConfig and BaseEntity were add

---
+++ user-service/src/main/java/com/vitkvsk/user_service/configurations/JpaAuditingConfig.java
@@ -0,0 +1,10 @@
+package com.vitkvsk.user_service.configurations;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
