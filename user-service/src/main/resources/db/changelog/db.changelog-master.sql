18c6168df0548dda67fc1a43676e0ede0c3734ba
Maksim Vitkouski <mvit4725@gmail.com>
2026-07-22 13:12:04 +0300

    Init liquibase DB schema was create

---
+++ user-service/src/main/resources/db/changelog/db.changelog-master.sql
@@ -0,0 +1,33 @@
+--liquibase formatted sql
+--changeset author:init
+
+CREATE SEQUENCE user_id_seq START WITH 1 INCREMENT BY 50;
+CREATE SEQUENCE payment_card_id_seq START WITH 1 INCREMENT BY 50;
+
+CREATE TABLE users (
+    id BIGINT NOT NULL DEFAULT nextval('user_id_seq') PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    surname VARCHAR(50) NOT NULL,
+    birth_date DATE NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    CONSTRAINT uq_user_email UNIQUE (email)
+);
+
+CREATE INDEX idx_users_surname_name ON users (surname, name);
+
+CREATE TABLE payment_cards (
+    id BIGINT NOT NULL DEFAULT nextval('payment_card_id_seq') PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    number VARCHAR(32) NOT NULL,
+    holder VARCHAR(100) NOT NULL,
+    expiration_date DATE NOT NULL,
+    active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    CONSTRAINT uq_card_number UNIQUE (number)
+);
+
+CREATE INDEX idx_cards_user_id ON payment_cards (user_id);
