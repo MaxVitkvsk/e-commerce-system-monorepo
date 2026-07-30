8413153a8e54e25083261299b248e1b47ce682bb
Maksim Vitkouski <mvit4725@gmail.com>
2026-07-30 03:57:47 +0300

    start cong auth-service was set up

---
+++ auth-service/src/main/java/com/vitkvsk/auth_service/AuthServiceApplication.java
@@ -0,0 +1,15 @@
+package com.vitkvsk.auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.resilience.annotation.EnableResilientMethods;
+
+@SpringBootApplication
+@EnableResilientMethods
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
+}
